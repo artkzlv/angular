9e4f059172efde44cf4ec43109bd4376f1ec7ebe
akozlov <akozlov@phoenixit.ru>
2026-09-04 16:59:22 +0300

    dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,37 @@
 import { Routes } from '@angular/router';
 import { PublicLayoutComponent } from './public/_layout/layout.component';
 import { PrivateLayoutComponent } from './private/_layout/layout.component';
+import { LogInComponent } from './public/pages/log-in/log-in';
+import { DashboardComponent } from './private/pages/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: 'public',
     component: PublicLayoutComponent,
+    children: [
+      {
+        path: 'log-in',
+        component: LogInComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'log-in',
+      },
+    ],
   },
   {
     path: 'private',
     component: PrivateLayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'dashboard',
+      },
+    ],
   },
   {
     path: '**',
