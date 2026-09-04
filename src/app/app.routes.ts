84f009d3604ea3a0784044fa23876f59a9d67a78
akozlov <akozlov@phoenixit.ru>
2026-09-04 17:12:45 +0300

    auth service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { PublicLayoutComponent } from './public/_layout/layout.component';
 import { PrivateLayoutComponent } from './private/_layout/layout.component';
 import { LogInComponent } from './public/pages/log-in/log-in';
 import { DashboardComponent } from './private/pages/dashboard/dashboard';
+import { authGuard } from './shared/guard/auth-guard';
 
 export const routes: Routes = [
   {
@@ -22,6 +23,7 @@ export const routes: Routes = [
   {
     path: 'private',
     component: PrivateLayoutComponent,
+    canActivate: [authGuard],
     children: [
       {
         path: 'dashboard',
---
+++ src/app/shared/guard/auth-guard.ts
@@ -0,0 +1,8 @@
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth';
+
+export const authGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  return authService.isAuthenticated;
+};
